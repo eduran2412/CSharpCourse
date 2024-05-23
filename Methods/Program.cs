57c22f307d15d5a8f021b42fecf5d9b0fe4bda21
Eren <erenduran2412@gmail.com>
2024-05-23 23:58:41 +0300

    Ref & Out Keywords

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -24,3 +24,45 @@ static int Add3(int n11, int n12 = 30) // default değerler metodun en sonunda o
     var res = n11 + n12;
     return res;
 }
+
+//-- Ref Keyword --
+int number1 = 20;
+int number2 = 100;
+
+var result2 = Add4(number1, number2);
+Console.WriteLine(result2);
+Console.WriteLine(number1);
+
+static int Add4(int number1, int number2)
+{
+    number1 = 30;
+    return number1 + number2;
+}
+
+int number3 = 20;//bu değerin metot içerisinde değişebileceği düşünülüyorsa ref olarak gönderilir.mesela kredi oranı sabit, bir müşteriye özel indirim olabilir.
+int number4 = 100;
+
+var result3 = Add5(ref number3, number4);
+Console.WriteLine(result3);
+Console.WriteLine(number3);
+
+static int Add5(ref int number3, int number4)
+{
+    number3 = 30;
+    return number3 + number4;
+}
+//--
+
+//-- Out Keyword --
+
+//mantık olarak ref ile aynı değer tipi referans tip gibi göndermek
+
+int number5;// ref içerisinde number set edilmiş olmalı, burada ona gerek yok int number; yeterlidir..
+int number6 = 100;
+var result4 = Add6(out number5, number6);
+
+static int Add6(out int number5, int number6)
+{
+    number5 = 30; // out kullanmadan önce metot içerisinde set edilmesi lazım!
+    return number5 + number6;
+}
